addfcdd36a3b77e04684d6fddda32753de5d5088
Navin GV <gnavin@amazon.com>
2024-03-21 16:51:35 +0530

    fix

---
+++ src/twoheaps/MedianOfStream.java
@@ -0,0 +1,75 @@
+package twoheaps;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+import static utils.Print.repeat;
+
+public class MedianOfStream {
+
+    PriorityQueue<Integer> firstPartMaxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+    PriorityQueue<Integer> secondPartMinHeap = new PriorityQueue<>(Comparator.naturalOrder());
+
+    public MedianOfStream() {
+
+    }
+
+    public void insertNum(int num) {
+        if (firstPartMaxHeap.isEmpty()) {
+            firstPartMaxHeap.add(num);
+            return;
+        }
+
+        if (num > firstPartMaxHeap.peek()) {
+            secondPartMinHeap.add(num);
+        } else {
+            firstPartMaxHeap.add(num);
+        }
+
+        if (Math.abs(firstPartMaxHeap.size()-secondPartMinHeap.size()) > 1 ) {
+            if (firstPartMaxHeap.size() > secondPartMinHeap.size()) {
+                Integer poll = firstPartMaxHeap.poll();
+                secondPartMinHeap.add(poll);
+            } else {
+                Integer poll = secondPartMinHeap.poll();
+                firstPartMaxHeap.add(poll);
+            }
+        }
+
+    }
+
+    public double findMedian() {
+
+        if (firstPartMaxHeap.size() == secondPartMinHeap.size()) {
+            return (firstPartMaxHeap.peek() + secondPartMinHeap.peek())/2.0d;
+        }
+
+        if (firstPartMaxHeap.size() > secondPartMinHeap.size()) {
+            return firstPartMaxHeap.peek();
+        } else {
+            return secondPartMinHeap.peek();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        // Driver code
+        int[] nums = {35, 22, 30, 25, 1};
+        MedianOfStream medianOfAges = null;
+        for(int i=0; i< nums.length; i++){
+            System.out.print(i+1);
+            System.out.print(".\tData stream: [");
+            medianOfAges = new MedianOfStream();
+            for(int j=0; j<=i; j++){
+                System.out.print(nums[j]);
+                if(j != i)
+                    System.out.print(", ");
+                medianOfAges.insertNum(nums[j]);
+            }
+            System.out.println("]");
+            System.out.println("\tThe median for the given numbers is: " + medianOfAges.findMedian());
+            System.out.println(repeat("-", 100));
+        }
+
+    }
+}
---
+++ src/twoheaps/ScheduleTask.java
@@ -0,0 +1,77 @@
+package twoheaps;
+
+import java.util.*;
+
+public class ScheduleTask {
+
+    static class Interval {
+        public int st;
+        public int end;
+        public int room;
+
+        public Interval(int st, int end) {
+            this.st = st;
+            this.end = end;
+        }
+
+        @Override
+        public String toString() {
+            return "Interval{" +
+                    "st=" + st +
+                    ", end=" + end +
+                    ", room=" + room +
+                    '}';
+        }
+    }
+
+    public static int tasks(List<List<Integer>> intervals) {
+
+        List<Interval> list = new ArrayList<>();
+
+        for (int i = 0; i < intervals.size(); i++) {
+            Interval interval = new Interval(intervals.get(i).get(0), intervals.get(i).get(1));
+            list.add(interval);
+        }
+
+        Collections.sort(list, Comparator.comparingInt(o -> o.st));
+
+        HashMap<Integer, Integer> roomIdToEndTimeMap = new HashMap<>();
+
+        List<List<Interval>> rooms = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            Interval interval = list.get(i);
+
+            boolean found = false;
+            for (Map.Entry<Integer, Integer> roomIdToEndTime : roomIdToEndTimeMap.entrySet()) {
+                if (interval.st >= roomIdToEndTime.getValue()) {
+                    Integer roomId = roomIdToEndTime.getKey();
+                    interval.room = roomId;
+                    rooms.get(roomId).add(interval);
+                    roomIdToEndTimeMap.put(roomId, interval.end);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                int roomId = rooms.size();
+                interval.room = roomId;
+                List<Interval> ll = new ArrayList<Interval>();
+                ll.add(interval);
+                rooms.add(ll);
+                roomIdToEndTimeMap.put(roomId, interval.end);
+            }
+        }
+
+        for (int i = 0; i < rooms.size(); i++) {
+            for (int j = 0; j < rooms.get(i).size(); j++) {
+                System.out.print(rooms.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+
+        return rooms.size();
+    }
+
+}
