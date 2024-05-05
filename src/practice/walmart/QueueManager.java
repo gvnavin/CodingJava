db25e360c4d895d3cde4dd280870b4ffc7507477
Navin GV <gnavin@amazon.com>
2024-05-05 12:17:44 +0530

    fix

--- src/practice/walmart/QueueManager.java
+++ src/practice/walmart/QueueManager.java
@@ -7,12 +7,12 @@ import java.util.Queue;
 public class QueueManager<T> {
 
     static class Holder {
-        static QueueManager<T> instance = new QueueManager<>();
+//        static QueueManager<T> instance = new QueueManager<>();
     }
 
-    public static QueueManager<T> getInstance() {
-        return Holder.instance;
-    }
+//    public static QueueManager<T> getInstance() {
+//        return Holder.instance;
+//    }
 
     HashMap<String,  Queue<T>> subscriptionKeyToQueueHashMap = new HashMap<>();
     private final Queue<T> q = new ArrayDeque<>();
