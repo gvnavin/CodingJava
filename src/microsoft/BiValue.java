1b27834ca9948d2af049081aec754c1591c7e5d4
Navin GV <gnavin@amazon.com>
2024-05-05 22:02:27 +0530

    fix

---
+++ src/microsoft/BiValue.java
@@ -0,0 +1,52 @@
+package microsoft;
+
+import java.util.HashMap;
+
+class BiValue {
+    public int solution(int[] inp) {
+        int beg = 0;
+        int end = 0;
+        int max = Integer.MIN_VALUE;
+
+        HashMap<Integer, Integer> cntMap = new HashMap<>();
+
+        while (end < inp.length) {
+            int endv = inp[end];
+            int endvCnt = cntMap.merge(endv, 1, (v1, v2) -> v1 + 1);
+            System.out.println("beg : " + beg + ", end : " + end + ", endvCnt = " + endvCnt);
+
+            if (cntMap.size() <= 2) {
+                System.out.println("if (cntMap.size() == 2) { = ");
+                int lsz = end - beg + 1;
+                System.out.println("lsz = " + lsz);
+                if (lsz > max) {
+                    max = lsz;
+                }
+            }
+
+            while (beg < inp.length && beg <= end && cntMap.size() > 2) {
+                int begv = inp[beg];
+                int lCnt = cntMap.merge(begv, 0, (v1, v2) -> v1 - 1);
+                if (lCnt == 0) {
+                    cntMap.remove(begv);
+                }
+                beg++;
+            }
+            end++;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        BiValue solution = new BiValue();
+        int ret = 0;
+//        ret = solution.solution(new int[]{4, 2, 2, 4, 2});
+//        System.out.println("ret = " + ret);
+//        ret = solution.solution(new int[]{1, 2, 3, 2});
+//        System.out.println("ret = " + ret);
+//        ret = solution.solution(new int[]{0, 5, 4, 4, 5, 12});
+//        System.out.println("ret = " + ret);
+        ret = solution.solution(new int[]{4, 4});
+        System.out.println("ret = " + ret);
+    }
+}
--- src/practice/walmart/Producer.java
+++ src/practice/walmart/Producer.java
@@ -2,18 +2,18 @@ package practice.walmart;
 
 public class Producer<T> {
 
-    private String producerId;
-
-    public Producer(String producerId) {
-        this.producerId = producerId;
-    }
-
-    ProduceStatus produce(T obj) {
-        produce(obj, QueueConstants.MAIN_QUEUE);
-    }
-
-    ProduceStatus produce(T obj, String subscriptionKey) {
-
-    }
+//    private String producerId;
+//
+//    public Producer(String producerId) {
+//        this.producerId = producerId;
+//    }
+//
+//    ProduceStatus produce(T obj) {
+//        produce(obj, QueueConstants.MAIN_QUEUE);
+//    }
+//
+//    ProduceStatus produce(T obj, String subscriptionKey) {
+//
+//    }
 
 }
