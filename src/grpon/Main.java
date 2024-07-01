2c3fc22bbbd0f5b98c2c9f9e40f212dcd50acb42
Navin GV <gnavin@amazon.com>
2024-07-01 10:46:21 +0530

    fix

---
+++ src/grpon/Main.java
@@ -0,0 +1,27 @@
+package grpon;
+
+import java.util.HashMap;
+
+public class Main {
+
+    /*
+
+// Design a data structure which will give us O(1) for
+// - insert(arg)
+// - remove (arg)
+// - contains(arg) - ValueSet from Hash
+// - getRandom() - Key, Value from Hash
+
+    * */
+
+
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int noOfElements = mp.size();
+        int insert = 100;
+        int indexToRemove = 5;
+//        mp.remove();
+//        mp.get(noOfElements-3)
+
+    }
+}
